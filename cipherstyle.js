606a8647234eb4caa21ec0cead73d3696c98cc00
scim <ryanedmondbeck@gmail.com>
2018-11-29 00:37:14 -0600

    cipher buttons
    tried to allow for buttons to change colors with cipherstyle.js but can't figure it out yet

---
+++ cipherstyle.js
@@ -0,0 +1,48 @@
+
+
+
+// 0
+function nClick() {
+	document.getElementById("n").style.color = "black";
+	document.getElementById("c").style.color = "white";
+	document.getElementById("v").style.color = "white";
+	document.getElementById("h").style.color = "white";
+	document.getElementById("n").style.background-color = "white";
+	document.getElementById("c").style.background-color = "black";
+	document.getElementById("v").style.background-color = "black";
+	document.getElementById("h").style.background-color = "black";
+
+}
+// 1
+function cClick() {
+	document.getElementById("c").style.color = "black";
+	document.getElementById("n").style.color = "white";
+	document.getElementById("v").style.color = "white";
+	document.getElementById("h").style.color = "white";
+	document.getElementById("c").style.background-color = "white";
+	document.getElementById("n").style.background-color = "black";
+	document.getElementById("v").style.background-color = "black";
+	document.getElementById("h").style.background-color = "black";
+}
+// 2
+function vClick() {
+	document.getElementById("v").style.color = "black";
+	document.getElementById("c").style.color = "white";
+	document.getElementById("n").style.color = "white";
+	document.getElementById("h").style.color = "white";
+	document.getElementById("v").style.background-color = "white";
+	document.getElementById("c").style.background-color = "black";
+	document.getElementById("n").style.background-color = "black";
+	document.getElementById("h").style.background-color = "black";
+}
+// 3
+function hClick() {
+	document.getElementById("h").style.color = "black";
+	document.getElementById("c").style.color = "white";
+	document.getElementById("v").style.color = "white";
+	document.getElementById("n").style.color = "white";
+	document.getElementById("h").style.background-color = "white";
+	document.getElementById("c").style.background-color = "black";
+	document.getElementById("v").style.background-color = "black";
+	document.getElementById("n").style.background-color = "black";
+}
